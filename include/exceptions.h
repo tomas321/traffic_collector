a9e2a95a7f725a591b5fac336a2ba937134d796a
Tomas Bellus <tomas.bellus@gmail.com>
2019-03-11 21:28:57 +0100

    extend custom exception to pass logging severity levels

--- include/exceptions.h
+++ include/exceptions.h
@@ -10,7 +10,19 @@
 #endif //TRAFFIC_COLLECTOR_EXCEPTIONS_H
 
 class ConfigurationError: public std::logic_error {
+private:
+    int severity_level;
 public:
+    ConfigurationError(const std::string &msg, int severity_level): std::logic_error(msg),  severity_level(severity_level) {}
     ConfigurationError(const std::string &msg): std::logic_error(msg) {}
     ConfigurationError(): std::logic_error("Invalid configuration value") {}
+};
+
+class CaptureError: public std::logic_error {
+private:
+    int severity_level;
+public:
+    CaptureError(const string &msg, int severity_level): logic_error(msg), severity_level(severity_level) {}
+    CaptureError(const std::string &msg): std::logic_error(msg) {}
+    CaptureError(): std::logic_error("Capturing raised an error") {}
 };
