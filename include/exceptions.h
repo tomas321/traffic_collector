2dda5f339d241d8f6ca765f220313bae03a4b016
Tomas Bellus <tomas.bellus@gmail.com>
2019-03-13 00:14:45 +0100

    refactor custom exceptions

--- include/exceptions.h
+++ include/exceptions.h
@@ -3,26 +3,35 @@
 //
 
 #include <stdexcept>
+#include <boost/log/trivial.hpp>
 
 #ifndef TRAFFIC_COLLECTOR_EXCEPTIONS_H
 #define TRAFFIC_COLLECTOR_EXCEPTIONS_H
 
 #endif //TRAFFIC_COLLECTOR_EXCEPTIONS_H
 
-class ConfigurationError: public std::logic_error {
+class ConfigurationError : public std::logic_error {
 private:
-    int severity_level;
+    boost::log::trivial::severity_level severity_level;
 public:
-    ConfigurationError(const std::string &msg, int severity_level): std::logic_error(msg),  severity_level(severity_level) {}
-    ConfigurationError(const std::string &msg): std::logic_error(msg) {}
-    ConfigurationError(): std::logic_error("Invalid configuration value") {}
+    ConfigurationError(const std::string &msg = "Invalid configuration value",
+                       boost::log::trivial::severity_level severity_level = boost::log::trivial::severity_level::error)
+            : std::logic_error(msg), severity_level(severity_level) {}
+
+    boost::log::trivial::severity_level get_severity() {
+        return severity_level;
+    };
 };
 
-class CaptureError: public std::logic_error {
+class CaptureError : public std::logic_error {
 private:
-    int severity_level;
+    boost::log::trivial::severity_level severity_level;
 public:
-    CaptureError(const string &msg, int severity_level): logic_error(msg), severity_level(severity_level) {}
-    CaptureError(const std::string &msg): std::logic_error(msg) {}
-    CaptureError(): std::logic_error("Capturing raised an error") {}
+    CaptureError(const string &msg = "Capturing raised an error",
+                 boost::log::trivial::severity_level severity_level = boost::log::trivial::severity_level::error)
+            : logic_error(msg), severity_level(severity_level) {}
+
+    boost::log::trivial::severity_level get_severity() {
+        return severity_level;
+    };
 };
