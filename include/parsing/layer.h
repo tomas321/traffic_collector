19c95474afb3cfd95b6de1ce2e02c9bc77626aa1
Tomas Bellus <tomas.bellus@gmail.com>
2019-03-27 00:31:44 +0100

    handle index errors with maps
    include logging to other classes
    
    fix bug in parsing attempts of unknown protocols following IP layer

--- include/parsing/layer.h
+++ include/parsing/layer.h
@@ -10,6 +10,8 @@
 #include <cstring>
 #include <map>
 
+#include <iostream>
+
 using namespace std;
 
 namespace Layers {
@@ -19,11 +21,12 @@ namespace Layers {
     enum Type {
         NONE = 0xffff,
         Unknown = 0x0000,
-        Ethernet = 0x0001,
+        Ethernet = 0x1111,
         ARP = 0x0806,
         IPv4 = 0x0800,
         IPv6 = 0x86dd,
-        ICMP = 0x3A,
+        ICMPv6 = 0x3A,
+        ICMP = 0x01,
         UDP = 0x11,
         TCP = 0x06,
         DNS = 0x35
@@ -32,32 +35,51 @@ namespace Layers {
     /**
      * Mapping supported types to string representations.
      */
-    const map<Type, const string> layer_string = {
-            {Ethernet, "ethernet"},
-            {ARP,      "arp"},
-            {IPv4,     "ipv4"},
-            {IPv6,     "ipv6"},
-            {ICMP,     "icmp"},
-            {UDP,      "udp"},
-            {TCP,      "tcp"},
-            {DNS,      "dns"},
-            {NONE,     "none"},
-            {Unknown,  "unknown"}
-    };
+     inline const string layer_string(Type type) {
+        const map<Type, const string> lmap = {
+                {Ethernet, "ethernet"},
+                {ARP,      "arp"},
+                {IPv4,     "ipv4"},
+                {IPv6,     "ipv6"},
+                {ICMPv6,   "icmpv6"},
+                {ICMP,     "icmp"},
+                {UDP,      "udp"},
+                {TCP,      "tcp"},
+                {DNS,      "dns"},
+                {NONE,     "none"},
+                {Unknown,  "unknown"}
+        };
+
+        try {
+            return lmap.at(type);
+        } catch (const std::out_of_range &e) {
+            cout << "unknown layer: " << int(type) << endl;
+            return lmap.at(Unknown);
+        }
+    }
 
     /**
      * Mapping headers to header lengths.
      */
-    const map<Type, const int> header_lengths = {
-            {Ethernet, 14},
-            {ARP,      28},
-            {IPv4,     20},
-            {IPv6,     24},
-            {ICMP,     4}, // temporary
-            {UDP,      8},
-            {TCP,      20},
-            {DNS,      12}
-    };
+     inline const int header_lengths(Type type) {
+        const map<Type, const int> lmap = {
+                {Ethernet, 14},
+                {ARP,      28},
+                {IPv4,     20},
+                {IPv6,     24},
+                {ICMP,     4}, // temporary
+                {UDP,      8},
+                {TCP,      20},
+                {DNS,      12}
+        };
+
+        try {
+            return lmap.at(type);
+        } catch (const std::out_of_range &e) {
+            cout << "unknown: " << int(type) << endl;
+            throw std::out_of_range("hdr length");
+        }
+    }
 }
 
 class Layer {
@@ -92,11 +114,11 @@ public:
     }
 
     void set_next(Layer *next) {
-        Layer::next = next;
+        this->next = next;
     }
 
     void set_previous(Layer *previous) {
-        Layer::previous = previous;
+        this->previous = previous;
     }
 };
 
