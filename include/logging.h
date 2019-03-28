b2e436e945bd45e1da08d48391c12bee08a8a6c7
Tomas Bellus <tomas.bellus@gmail.com>
2019-03-28 22:56:51 +0100

    DOC: logging header file documentation

--- include/logging.h
+++ include/logging.h
@@ -10,6 +10,9 @@
 #include <map>
 
 
+/**
+ * Log level enumeration to keep backward compatibility.
+ */
 enum log_level {
     debug,
     info,
@@ -23,16 +26,45 @@ enum log_level {
 
 class Logging {
 private:
+    /**
+     * Mapping of previous (boost) log level names with syslog log levels.
+     */
     static const std::map<log_level, int> log_map;
     static Logging *logger;
-    Logging(int lvl);
+
+    /**
+     * Open syslog session (descriptor) and set minimum log level.
+     * @param lvl Minimum log level.
+     */
+    explicit Logging(int lvl);
 
 public:
+    /**
+     * Closes the log level descriptor.
+     */
     virtual ~Logging();
 
 public:
+    /**
+     * Initialize the logging descriptor with minimum loging level.
+     * @param severity_level Minimum log level.
+     */
     static void init(int severity_level);
+
+    /**
+     * Log message of given severity level.
+     *
+     * @param level Message severity level (log_level).
+     * @param msg Log message.
+     */
     static void log(log_level level, const char *msg);
+
+    /**
+     * Log message of given severity level.
+     *
+     * @param level Message severity level (log_level).
+     * @param msg Log message.
+     */
     static void log(log_level level, const std::string &msg);
 };
 
