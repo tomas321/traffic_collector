12e23d51bc50ec659f28fc6543796474e1583e71
Tomas Bellus <tomas.bellus@gmail.com>
2019-04-02 13:38:24 +0200

    add simple harmonization class for resolving IP address to country codes

--- include/harmonization.h
+++ include/harmonization.h
@@ -6,10 +6,36 @@
 #define TRAFFIC_COLLECTOR_HARMONIZATION_H
 
 
+#include <string>
+#include <GeoIP.h>
+
+#define GEOIP_FILEPATH "resources/geoip/GeoIP.dat"
+
 class Harmonization {
-/** TODO: this class provides harmonization tools (geoip)
- *        therefore it must have a contructor to open a local database
- */
+private:
+    std::string geoip_filepath;
+    GeoIP *gi;
+
+public:
+    /**
+     * Initialize geoip database for searching.
+     *
+     * @param geoip_filepath Path to GeoIP.dat file
+     */
+    explicit Harmonization(const std::string &geoip_filepath);
+
+    /**
+     * Initialize geoip database for searching with default path to GeoIP.dat file
+     */
+    Harmonization();
+
+    /**
+     * Get geoip info for IP address.
+     *
+     * @param ip IP address to be queried.
+     * @return Geoip data structure.
+     */
+    std::string geoip(const std::string &ip);
 };
 
 
