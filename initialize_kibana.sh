f4768a60e42ac91a0f2fa076ecedfbe49862b76a
Tomas Bellus <tomas.bellus@gmail.com>
2019-04-10 00:14:48 +0200

    fix saved_objects json reading
    The escaped characters in json values were escaped and the backslashes were not preserved. A fix is to read line with -r option and utilizing echo to to preserve bashslashes with option -e.

--- initialize_kibana.sh
+++ initialize_kibana.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 if [[ $# != 1 ]]; then
     echo "Error: 'requires 1 argument': USAGE: $0 <host>" > /dev/stderr
     exit 1
@@ -25,8 +24,6 @@ else
     echo "Warning: 'missing netcat': unable to verify connection to daemons" > /dev/stderr
 fi
 
-exit 0
-
 script_dir="$(dirname $0)"
 template_path="$script_dir/resources/elk/bp_index_template.json"
 objects_path="$script_dir/resources/elk/saved_objects.json"
@@ -37,17 +34,17 @@ template_name=${tmp%.*}
 # read json file ("$1") to a single json string
 function readfile() {
     local data=""
-    while read line; do
+    while read -r line; do
         data="$data$line"
     done < "$1"
-    echo "$data"
+    echo -e ${data}
 }
 
 template_data="$(readfile ${template_path})"
-curl -X PUT "$host:9200/_template/$template_name" -H "Content-Type: application/json" -d "$template_data" 1>/dev/null 2>/dev/stderr
+curl -X PUT "$host:9200/_template/$template_name" -H "Content-Type: application/json" -d "$template_data" 1>/dev/null
 
 
 objects_data="$(readfile ${objects_path})"
-curl -X POST "$host:5601/api/saved_objects/_bulk_create?overwrite=true" -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d "$objects_data" 1>/dev/null 2>/dev/stderr
+curl -X POST "$host:5601/api/saved_objects/_bulk_create?overwrite=true" -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d "$(echo -e ${objects_data})" 1>/dev/null
 
 exit 0
